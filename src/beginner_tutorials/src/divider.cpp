365e98509289a0d10f6fe4e6a626d2e25fe59037
Roberts <robertskomarovskis@gmail.com>
2019-03-13 13:35:40 +0200

    Divider node, which divides float msg from subscribed topic, divides it by 2 and advertises the customized message on another topic

---
+++ src/beginner_tutorials/src/divider.cpp
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "std_msgs/Float32.h"
+
+void dividerCallback(std_msgs::Float32 msg){
+	ros::NodeHandle m;
+
+	ros::Publisher divider_pub = m.advertise<std_msgs::Float32>("TV",1000);
+	msg.data = msg.data / 2;
+	ros::Rate loop_rate(1);
+	while(ros::ok()){
+		ROS_INFO("I heard: [%.2f]", msg.data);
+		ros::Duration(1).sleep();
+		divider_pub.publish(msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+}
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "divider");
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("chatter",1000, dividerCallback);
+	ros::spin();
+	return 0;
+}
