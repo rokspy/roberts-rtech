a2abf0bc318d93975d8d496d5db6a7190a398574
rt-user <robertskomarovskis@gmail.com>
2019-03-14 13:48:01 +0200

    Adding modified divider cpp file and 3 lab work(beginning)

--- src/beginner_tutorials/src/divider.cpp
+++ src/beginner_tutorials/src/divider.cpp
@@ -1,19 +1,20 @@
 #include "ros/ros.h"
 #include "std_msgs/Float32.h"
 
-void dividerCallback(std_msgs::Float32 msg){
-	ros::NodeHandle m;
 
-	ros::Publisher divider_pub = m.advertise<std_msgs::Float32>("TV",1000);
+void dividerCallback(std_msgs::Float32 msg){
+
 	msg.data = msg.data / 2;
 	ros::Rate loop_rate(1);
-	while(ros::ok()){
-		ROS_INFO("I heard: [%.2f]", msg.data);
-		ros::Duration(1).sleep();
-		divider_pub.publish(msg);
-		ros::spinOnce();
-		loop_rate.sleep();
-	}
+	divider_pub.publish(msg);
+	ROS_INFO("I heard: [%.2f]", msg.data);
+//	while(ros::ok()){
+//		ROS_INFO("I heard: [%.2f]", msg.data);
+//		ros::Duration(1).sleep();
+//		divider_pub.publish(msg);
+//		ros::spinOnce();
+//		loop_rate.sleep();
+//	}
 
 }
 
@@ -21,8 +22,11 @@ int main(int argc, char **argv){
 
 	ros::init(argc, argv, "divider");
 	ros::NodeHandle n;
+	ros::NodeHandle m;
 
+	ros::Publisher divider_pub = m.advertise<std_msgs::Float32>("TV",1000);
 	ros::Subscriber sub = n.subscribe("chatter",1000, dividerCallback);
+
 	ros::spin();
 	return 0;
 }
