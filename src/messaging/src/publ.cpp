f1709166a97875be6cc3d28c060df73596ce36b3
Roberts <robertskomarovskis@gmail.com>
2019-04-18 01:09:59 +0300

    Changes before week 33 practical

--- src/messaging/src/publ.cpp
+++ src/messaging/src/publ.cpp
@@ -4,19 +4,22 @@
 #include <sstream>
 #include <cstdlib>
 
+
+#define PI 3.14159
+
 float RandomFloat(float a, float b){
 	float random = ((float) rand()) / (float) RAND_MAX;
 	float diff = b - a;
 	float r = random * diff;
 	return a + r;
-
+ 
 }
 
 
 int main (int argc, char **argv){
-
+	
 	ros::init(argc, argv, "publ");
-
+		
 	ros::NodeHandle n;
 
 	ros::Publisher publii_pub = n.advertise<sensor_msgs::Range>("publii", 1000);
@@ -29,7 +32,9 @@ int main (int argc, char **argv){
 	msg.min_range = 0.02;
 	msg.max_range = 4.00;
 	msg.header.frame_id = "base_link";
-		
+	msg.field_of_view = PI/3;
+	msg.radiation_type = 0;
+
 	while (ros::ok()){
 
 		//ROS_INFO("%.2f", msg.range);
