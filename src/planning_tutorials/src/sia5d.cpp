6bcbfde37a8b8db4a02870a63378ec0c92c004cc
Roberts <robertskomarocskis@gmail.com>
2019-05-16 14:50:24 +0300

    Practical 9

--- src/planning_tutorials/src/sia5d.cpp
+++ src/planning_tutorials/src/sia5d.cpp
@@ -22,23 +22,10 @@ int main(int argc, char** argv)
 
   moveit::planning_interface::MoveGroupInterface move_group(PLANNING_GROUP);
 
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
 
-  namespace rvt = rviz_visual_tools;
-  
-  moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
+ // moveit_visual_tools::MoveItVisualTools visual_tools("base_link");
 
-  Eigen::Affine3d text_pose = Eigen::Affine3d::Identity();
-  text_pose.translation().z() = 1;	
-
-  ROS_INFO_NAMED("tutorial", "Reference frame: %s", move_group.getPlanningFrame().c_str());
-
-  ROS_INFO_NAMED("tutorial", "End effector link: %s", move_group.getEndEffectorLink().c_str());
-
-  // Start the demo
-  // ^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
   geometry_msgs::Pose target_pose1 = move_group.getCurrentPose().pose;
@@ -55,25 +42,19 @@ int main(int argc, char** argv)
   
   target_pose1.position.z += 0.2;
   target_pose1.position.y += 0.2;
-
   waypoints.push_back(target_pose1);  // up and left
   
-  move_group.setMaxVelocityScalingFactor(0.1);
+
 
   moveit_msgs::RobotTrajectory trajectory;
+  
+
+
   const double jump_threshold = 0.0;
   const double eef_step = 0.01;
 
+
   double fraction = move_group.computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
-
-  ROS_INFO_NAMED("tutorial", "Visualizing plan 4 (Cartesian path) (%.2f%% acheived)", fraction * 100.0);
-
-  visual_tools.publishText(text_pose, "Motoman sia5d", rvt::RED, rvt::XLARGE);
-  visual_tools.publishPath(waypoints, rvt::LIME_GREEN, rvt::SMALL);
-  for (std::size_t i = 0; i < waypoints.size(); ++i)
-    visual_tools.publishAxisLabeled(waypoints[i], "pt" + std::to_string(i), rvt::SMALL);
-  move_group.move();
-  visual_tools.trigger();
 
   my_plan.trajectory_=trajectory;
   move_group.execute(my_plan);
